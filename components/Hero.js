032f9d20f8ee262ff21d095a19e29831a5457ffe
Ashutosh Bhandari <ashutoshbhandari5@gmail.com>
2022-11-03 17:59:26 +0545

    removed duplicates

--- components/Hero.js
+++ components/Hero.js
@@ -1,29 +1,27 @@
 import React from "react";
+import { benefits, company } from "../utils/lists";
 
 const Hero = () => {
+  const renderBullets = (item) => {
+    return (
+      <div className={item.css}>
+        <h3>{item.header}</h3>
+        <ul>
+          {item.list.map((el, i) => {
+            return <li key={i}>{el}</li>;
+          })}
+        </ul>
+      </div>
+    );
+  };
+
   return (
     <div className="hero">
       <div className="content">
         <h1>Logistics Company</h1>
         <div>
-          <div className="benefits">
-            <h3>Benefits</h3>
-            <ul>
-              <li>Streamline analytics processes</li>
-              <li>Embed BI and predictive planning</li>
-              <li>Turn insights into action</li>
-              <li>Visualize your decisions</li>
-            </ul>
-          </div>
-          <div className="company">
-            <h3>Our Company</h3>
-            <ul>
-              <li>Streamline analytics processes</li>
-              <li>Embed BI and predictive planning</li>
-              <li>Turn insights into action</li>
-              <li>Visualize your decisions</li>
-            </ul>
-          </div>
+          {renderBullets(benefits)}
+          {renderBullets(company)}
         </div>
       </div>
     </div>
---
+++ utils/lists.js
@@ -0,0 +1,20 @@
+export const benefits = {
+  header: "Benefits",
+  css: "bullets bg-green",
+  list: [
+    "Streamline analytics processes",
+    "Embed BI and predictive planning",
+    "Turn insights into action",
+    "Visualize your decisions",
+  ],
+};
+export const company = {
+  header: "Our Company",
+  css: "bullets bg-black",
+  list: [
+    "Streamline analytics processes",
+    "Embed BI and predictive planning",
+    "Turn insights into action",
+    "Visualize your decisions",
+  ],
+};
